fbc2ef460eef4013af80334fedfe19a387553706
harrydengchao <dengchao2056@126.com>
2015-09-14 17:02:46 +0800

    检测字符串顺序排列

--- example20150914.py
+++ example20150914.py
@@ -2,12 +2,15 @@
 #coding:utf-8
 ###################################################
 #### check string seriation(检测字符串顺序排列)####
-##################################################
+#### 待测字符串存在 example20150914.txt ###########
+###################################################
+
+str_file_PATH = 'example20150914.txt'
 
 def str_seriation_check(str):
 #       "-*-检测字符串顺序排列-*-"
 #       print str_seriation_check.__doc__
-        print "-*-检测字符串顺序排列-*-"
+        print "-*-检测字符串顺序排列-*-{0}".format(str).strip('\n')
         if len(str) < 2:
                 return "字符串太短"
         for i in xrange(len(str)):
@@ -18,5 +21,10 @@ def str_seriation_check(str):
                                 return "Fail,字符串不是按顺序排列的. 第 {} 位大于第 {} 位.".format(i, i+1)
         return "OK,字符串是按顺序排列的."
 
+def read_file(file_str):
+        fr = open(file_str)
+        for read_line in fr:
+                print str_seriation_check(read_line)
+
 if __name__ == "__main__":
-        print str_seriation_check('abcdfe')
+        read_file(str_file_PATH)
