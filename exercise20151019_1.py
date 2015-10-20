43d5249362175d52b4c7376ecc47a62f158cce45
harrydengchao <dengchao2056@126.com>
2015-10-20 10:00:11 +0800

    一个螺旋数字矩阵，由外向里旋转

--- exercise20151019_1.py
+++ exercise20151019_1.py
@@ -4,11 +4,13 @@
 '''
 一个螺旋数字矩阵，由外向里旋转
 输入一个数字n，打印出如下数字：
-  1   2   3   4
- 12  13  14   5
- 11  16  15   6
- 10   9   8   7
-这是输入4的情况。
+  1   2   3   4   5   6
+ 20  21  22  23  24   7
+ 19  32  33  34  25   8
+ 18  31  36  35  26   9
+ 17  30  29  28  27  10
+ 16  15  14  13  12  11
+这是输入6的情况。
 '''
 
 def num_set(num):
@@ -19,51 +21,50 @@ def num_set(num):
     count = 1       #初始圈数
 #---direction = ("E", "S", "W", "N")
 #---方向操作    右、下、左、上
-
+    direction = "E"     #初始化执行方向
     while True:
-        direction = "E"     #初始化执行方向
 #-------向右走
         while direction == "E":
-            if init_Y > num - count:
+            set_list[init_X][init_Y] = value
+            if init_Y > num - count - 1:
                 direction = "S"
-                init_Y -= 1
-                value -= 1
+                continue
             else:
-                set_list[init_X][init_Y] = value
                 init_Y += 1
                 value += 1
+
 #-------向下走
         while direction == "S":
-            if init_X > num - count:
+            set_list[init_X][init_Y] = value
+            if init_X > num - count - 1:
                 direction = "W"
-                init_X -= 1
-                value -= 1
+                continue
             else:
-                set_list[init_X][init_Y] = value
                 init_X += 1
                 value += 1
+
 #-------向左走
         while direction == "W":
-            if init_Y < count - 1:
+            set_list[init_X][init_Y] = value
+            if init_Y < count:
                 direction = "N"
-                init_Y += 1
-                value -= 1
+                continue
             else:
-                set_list[init_X][init_Y] = value
                 init_Y -= 1
                 value += 1
+
 #-------向上走
         while direction == "N":
-            if init_X < count:
+            set_list[init_X][init_Y] = value
+            if init_X < count + 1:
                 direction = "E"
-                init_X += 1
-                value -= 1
+                continue
             else:
-                set_list[init_X][init_Y] = value
                 init_X -= 1
                 value += 1
+
 #-------循环控制条件
-        if count < pow(num, 2):
+        if value < pow(num, 2):
             count += 1
         else:
 #-------格式化打印输出
@@ -74,4 +75,8 @@ def num_set(num):
             break
 #############################################
 #-------函数调用
-num_set(4)
+if __name__ == '__main__':
+    for i in xrange(3,11,1):
+        num_set(i)
+        print
+#num_set(5)
