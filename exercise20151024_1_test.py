825ea791b0b258405eaab2fb9ce322d12333a3df
harrydengchao <dengchao2056@126.com>
2015-11-02 15:12:38 +0800

    商品定价，测试用例

--- exercise20151024_1_test.py
+++ exercise20151024_1_test.py
@@ -1,24 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 ### exercise20151024_1_test.py
-'''
-商品定价，测试用例。
-'''
+'''商品定价，测试用例。'''
 import unittest
-from exercise20151024_1 import *
+from exercise20151024_1 import Everyday_deco,Weekend_deco,pricing,price
 
-class TestPrice(object):
+class TestPrice(unittest.TestCase):
     """docstring for TestPrice"""
     def test_init(self):
         d = pricing(HUAWEI=3000, Iphone=5288, Thinkpad=7000)
-        self.assertEqual(d.HUAWEI, 3000)
-        self.assertEqual(d.Iphone, 5288)
-        self.assertEqual(d.Thinkpad, 7000)
-        self.assertTrue(isinstance(d, dict))
+        self.assertEqual(d["HUAWEI"], "2380.00")
 
-    def test_error(self):
-        with self.assertRaises(TypeError):
-            pricing(HUAWEI=3000)
-            pricing("HUAWEI")
 if __name__ == '__main__':
     unittest.main()
